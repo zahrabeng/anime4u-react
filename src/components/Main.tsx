1be6d50589803a292b5259d6bc6a031917f2e532
Zahra <bghzahra@outlook.com>
2022-07-09 22:10:07 +0100

    imported interface and mapped through images

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,23 +1,29 @@
 //import axios from "axios";
-import { useEffect } from "react";
+import allData from "./Interfaces";
+import { useEffect, useState } from "react";
 
-export default function Main ():JSX.Element{
+export default function Main(): JSX.Element {
+  const [pageData, setPageData] = useState<allData[]>([]);
+  const [pageRefresh, setPageRefresh] = useState<boolean>(false);
 
-    const jikanAPI = "https://api.jikan.moe/v4/anime?q=" //?q= added to the end of the API to query
-    
+  const jikanAPI = "https://api.jikan.moe/v4/anime?q="; //?q= added to the end of the API to query
+
+  useEffect(() => {
     async function getAllData() {
-        const result = await fetch(`${jikanAPI}&order_by=popularity&sort=desc&page=1&rating=g`)
-        const jsonBody = await result.json()
-        console.log(jsonBody.data)
+      const result = await fetch(
+        `${jikanAPI}&order_by=popularity&sort=desc&page=1&rating=g`
+      );
+      const jsonBody = await result.json();
+      setPageData(jsonBody.data);
     }
+    getAllData();
+  }, []);
 
-    useEffect (() => {
-        getAllData()
-    }, []) 
- 
-    return (
-        <>
+//   console.log(pageData);
 
-        </>
-    )
+  return (
+    <>
+      {pageData.map((data:allData) => <img src={data.images.jpg.image_url} key={data.mal_id}></img>)}
+    </>
+  );
 }
