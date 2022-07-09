1400c779a23aae969d5f5d691492a8aba0a17909
Zahra <bghzahra@outlook.com>
2022-07-09 15:31:45 +0100

    added api link to a variable for reusability

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,8 +3,10 @@ import { useEffect } from "react";
 
 export default function Main ():JSX.Element{
 
+    const jikanAPI = "https://api.jikan.moe/v4/anime?q=" //?q= added to the end of the API to query
+    
     async function getAllData() {
-        const result = await fetch("https://api.jikan.moe/v4/anime?q=&order_by=popularity&sort=desc&page=1&rating=g")
+        const result = await fetch(`${jikanAPI}&order_by=popularity&sort=desc&page=1&rating=g`)
         const jsonBody = await result.json()
         console.log(jsonBody.data)
     }
@@ -12,9 +14,10 @@ export default function Main ():JSX.Element{
     useEffect (() => {
         getAllData()
     }, []) 
-
+ 
     return (
         <>
+
         </>
     )
 }
