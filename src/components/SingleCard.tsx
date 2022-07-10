a55320e6e01e84b96bcd8fd8782b26b50b536ab4
Zahra <bghzahra@outlook.com>
2022-07-10 17:44:51 +0100

    added back button with useNavigate function

--- src/components/SingleCard.tsx
+++ src/components/SingleCard.tsx
@@ -1,10 +1,14 @@
+import {useNavigate} from "react-router-dom"
+
 interface Iprops {
   cardId: number;
 }
 
 export default function SingleCard(props: Iprops): JSX.Element {
+const navigate = useNavigate()
   return (
-    <>
+    <> 
+    <button onClick={()=> navigate(-1)}>Back</button>
       <p>Hello world {props.cardId}</p>
     </>
   );
