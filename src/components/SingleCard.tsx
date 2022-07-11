d8215ba0775626933a0996d86308bcc6da6bbe23
Zahra <bghzahra@outlook.com>
2022-07-11 12:02:30 +0100

    added data to single card component

--- src/components/SingleCard.tsx
+++ src/components/SingleCard.tsx
@@ -10,11 +10,19 @@ export default function SingleCard(props: Iprops): JSX.Element {
   const navigate = useNavigate();
 
   const singleData = props.allData.find((eachObj) => eachObj.mal_id === props.cardId)
-  
+  console.log(singleData)
   return (
     <>
-      <button onClick={() => navigate(-1)}>Back</button>
-      <p>Hello world {props.cardId}</p>
-    </>
+    <button onClick={() => navigate(-1)}>Back</button>
+    <h1>{singleData?.title}</h1>
+    <h2>{singleData?.title_japanese}</h2>
+    <img src={singleData?.images.jpg.image_url}></img>
+    <p>{singleData?.synopsis}</p>
+    <div>
+        <p>Type: {singleData?.type}</p>
+        <p>Episodes: {singleData?.episodes}</p>
+        <p>Genre: {singleData?.genres.map((obj) => ` ✨${obj.name}`)}</p>
+    </div>
+    </> 
   );
 }
